b0beab4ce1353c18958478a6806ac5b8069b6a4b
steven.le1510 <steven.le1510@gmail.com>
2026-03-04 16:11:01 +0000

    update new cloudfalre d1 database

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -3,6 +3,6 @@ import { drizzle } from "drizzle-orm/d1";
 import type { HonoEnv } from "../types/app";
 import * as schema from "./schema";
 
-export const getDb = (bindings: HonoEnv["Bindings"]) => drizzle(bindings.backend, { schema });
+export const getDb = (bindings: HonoEnv["Bindings"]) => drizzle(bindings.DB, { schema });
 
 export type Database = ReturnType<typeof getDb>;
